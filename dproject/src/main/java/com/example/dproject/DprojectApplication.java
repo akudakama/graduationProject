c7420002c0534be68f37222d33aeb1b38ca89947
akudakama <akugahao@gmail.com>
2025-07-06 17:15:20 +0300

    01

---
+++ dproject/src/main/java/com/example/dproject/DprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DprojectApplication.class, args);
+	}
+
+}
